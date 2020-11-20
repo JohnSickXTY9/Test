30539d7769ddeef800aa86cf8dd3309b9614bf84
JohnSickXTY9 <manhunter223@gmail.com>
2020-11-20 15:05:28 +0800

    Studied JS (Again)
    Simple JS stuff

---
+++ HTML Stuff/JS-Basics/index.js
@@ -0,0 +1,14 @@
+// Script starts here <<<
+
+// Performing a task
+function greet(name) {
+  console.log("Hello " + name);
+}
+
+// Calculating a val
+function square(number) {
+  return number * number;
+}
+
+greet("John"); // Should return "Hello John"
+console.log(square(2)); // Should return "4"
